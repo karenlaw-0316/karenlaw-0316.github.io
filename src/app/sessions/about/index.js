36b3872b3d817e415f06aefefe1709442581b203
karenlaw-0316 <lawhiuyankarenlaw@gmail.com>
2023-07-15 14:49:25 +0800

    add in about and subtitle and profilepic

--- src/app/sessions/about/index.js
+++ src/app/sessions/about/index.js
@@ -5,6 +5,20 @@ export default function About() {
 	return (
 		<div className="About__main">
 			<Title name="ABOUT ME"></Title>
+			<div className="About__description">Let's know more about me!</div>
+			<div className="About__contentWrapper">
+				<div className="About__profilePic">
+					<img
+						className="About__profilePic"
+						src={"/assets/ProfilePic.jpg"}
+						alt="Profile picture"
+					></img>
+				</div>
+				<div className="About__bioSection">
+					<div className="About__bioSection__littleBio"></div>
+					<div className="About__bioSection__mySkillSet"></div>
+				</div>
+			</div>
 		</div>
 	);
 }
