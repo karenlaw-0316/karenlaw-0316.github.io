e9edaaf9b4a677656291a83453fbb417cc78f5de
karenlaw-0316 <lawhiuyankarenlaw@gmail.com>
2023-07-15 15:17:53 +0800

    update about section

--- src/app/components/subtitle/index.js
+++ src/app/components/subtitle/index.js
@@ -1,1 +1,24 @@
-export default function Subtitle() {}
+import "./style.css";
+
+export default function Subtitle({ name }) {
+	return (
+		<div className="Subtitle__main">
+			<div className="Subtitle__name">{name}</div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="86"
+				height="4"
+				viewBox="0 0 86 4"
+				fill="none"
+			>
+				<path
+					d="M2 2H84"
+					stroke="#0079D1"
+					stroke-width="4"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+			</svg>
+		</div>
+	);
+}
--- src/app/sessions/about/index.js
+++ src/app/sessions/about/index.js
@@ -1,5 +1,8 @@
 import Title from "@/app/components/title";
+import Subtitle from "@/app/components/subtitle";
 import "./style.css";
+
+const skillsets = ["JavaScript", "Java", "Python", "R"];
 
 export default function About() {
 	return (
@@ -15,8 +18,26 @@ export default function About() {
 					></img>
 				</div>
 				<div className="About__bioSection">
-					<div className="About__bioSection__littleBio"></div>
-					<div className="About__bioSection__mySkillSet"></div>
+					<div className="About__bioSection__littleBio">
+						<Subtitle name="LITTLE BIO"></Subtitle>
+						<div className="About__bioSection__littleBio__content"></div>
+					</div>
+
+					<div className="About__bioSection__mySkillSet">
+						<Subtitle name="MY SKILLSET"></Subtitle>
+						<div className="About__bioSection__mySkillSet__wrapper">
+							{skillsets.map((skillset, i) => {
+								return (
+									<div
+										className="About__bioSection__mySkillSet__tag"
+										key={skillset}
+									>
+										{skillset}
+									</div>
+								);
+							})}
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
