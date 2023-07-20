21cfe3fc3c94a31a1794069710c067f1c6db6fae
karenlaw-0316 <lawhiuyankarenlaw@gmail.com>
2023-07-20 23:44:33 +0800

    update hamburger menu

--- src/app/components/header/hamburger/index.js
+++ src/app/components/header/hamburger/index.js
@@ -2,19 +2,54 @@
 
 import "./style.css";
 import { slide as Menu } from "react-burger-menu";
+import Link from "next/link";
 
 export default function HamburgerMenu() {
+	function scrollToView(elementId) {
+		if (window.location.pathname.includes("projectDetailPage")) {
+			window.location.replace(`/#${elementId}`);
+		} else {
+			const element = document.getElementById(elementId);
+			element.scrollIntoView();
+		}
+	}
 	return (
 		<Menu>
-			<a id="home" className="menu-item" href="/">
-				Home
-			</a>
-			<a id="about" className="menu-item" href="/about">
-				About
-			</a>
-			<a id="contact" className="menu-item" href="/contact">
-				Contact
-			</a>
+			<Link className="HamburgerHeader__name" href="/">
+				KAREN, LAW HIU YAN
+			</Link>
+			<div className="HamburgerHeader__sections">
+				<div
+					className="HamburgerHeader__sections_elements"
+					onClick={() => scrollToView("intro")}
+				>
+					HOME
+				</div>
+				<div
+					className="HamburgerHeader__sections_elements"
+					onClick={() => scrollToView("about")}
+				>
+					ABOUT
+				</div>
+				<div
+					className="HamburgerHeader__sections_elements"
+					onClick={() => scrollToView("experience")}
+				>
+					EXPERIENCE
+				</div>
+				<div
+					className="HamburgerHeader__sections_elements"
+					onClick={() => scrollToView("project")}
+				>
+					PROJECT
+				</div>
+				<div
+					className="HamburgerHeader__sections_elements"
+					onClick={() => scrollToView("contact")}
+				>
+					CONTACT
+				</div>
+			</div>
 		</Menu>
 	);
 }
