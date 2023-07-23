80820624f480be88ffcec42b6826e58d3e3b1915
karenlaw-0316 <lawhiuyankarenlaw@gmail.com>
2023-07-23 14:09:00 +0800

    update contact session

--- src/app/sessions/contact/index.js
+++ src/app/sessions/contact/index.js
@@ -11,10 +11,33 @@ export default function Contact() {
 			<div className="Contact__name">
 				<Highlighter
 					highlightClassName="Contact__name__highlight"
-					searchWords={["CONTACT ME"]}
+					searchWords={["CONTACT ME :)"]}
 					autoEscape={true}
-					textToHighlight="CONTACT ME"
+					textToHighlight="CONTACT ME :)"
 				/>
+			</div>
+			<div className="Contact__icon__wrapper">
+				<a href="https://github.com/karenlaw-0316" target="_blank">
+					<img
+						src={"/assets/icon/github_icon.png"}
+						className="Contact__icon"
+					/>
+				</a>
+				<a
+					href="https://www.linkedin.com/in/karen-law-a693711b3/"
+					target="_blank"
+				>
+					<img
+						src={"/assets/icon/linkedin_icon.png"}
+						className="Contact__icon"
+					/>
+				</a>
+				<a href="mailto:lawhiuyankarenlaw@gmail.com" target="_blank">
+					<img
+						src={"/assets/icon/email_icon.png"}
+						className="Contact__icon"
+					/>
+				</a>
 			</div>
 		</div>
 	);
